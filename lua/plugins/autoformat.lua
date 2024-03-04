3a4a05c947da98d85870f856f26998a0cd73c448
marinjuricev <mjuric00@fesb.hr>
2024-03-04 08:46:01 +0100

    chore: Format code in autoformat.lua

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -22,7 +22,7 @@ return {
       if not _augroups[client.id] then
         local group_name = 'kickstart-lsp-format-' .. client.name
         local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-                _augroups[client.id] = id
+        _augroups[client.id] = id
       end
 
       return _augroups[client.id]
