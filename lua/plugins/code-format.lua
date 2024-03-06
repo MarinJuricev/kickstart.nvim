88ada96239cabeee3963484dab47c357cc766b29
marinjuricev <mjuric00@fesb.hr>
2024-03-06 22:36:46 +0100

    chore: Add TODO comments

--- lua/plugins/code-format.lua
+++ lua/plugins/code-format.lua
@@ -10,7 +10,8 @@ return {
     main = 'ibl',
     opts = {},
   },
-
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 }
