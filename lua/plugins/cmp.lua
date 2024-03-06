7e855ee111607498088ef60bb0abdc7dff237f1a
marinjuricev <mjuric00@fesb.hr>
2024-03-06 22:19:45 +0100

    chore: Update cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,6 @@
-return {
-  -- Autocompletion
+return { -- Autocompletion
   'hrsh7th/nvim-cmp',
-  event = "InsertEnter",
+  event = 'InsertEnter',
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
     {
@@ -10,7 +9,7 @@ return {
         -- Build Step is needed for regex support in snippets
         -- This step is not supported in many windows environments
         -- Remove the below condition to re-enable on windows
-        if vim.fn.has 'win32' == 1 then
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
         return 'make install_jsregexp'
@@ -18,19 +17,22 @@ return {
     },
     'saadparwaiz1/cmp_luasnip',
 
-    -- Adds LSP completion capabilities
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
 
-    -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets',
+    -- If you want to add a bunch of pre-configured snippets,
+    --    you can use this plugin to help you. It even has snippets
+    --    for various frameworks/libraries/etc. but you will have to
+    --    set up the ones that are useful for you.
+    -- 'rafamadriz/friendly-snippets',
   },
-  opts = function()
-    -- [[ Configure nvim-cmp ]]
+  config = function()
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
     cmp.setup {
@@ -39,35 +41,44 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-      completion = {
-        completeopt = 'menu,menuone,noinsert',
-      },
+      completion = { completeopt = 'menu,menuone,noinsert' },
+
+      -- For an understanding of why these mappings were
+      -- chosen, you will need to read `:help ins-completion`
+      --
+      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
+        -- Select the [n]ext item
         ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- Select the [p]revious item
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+        -- Accept ([y]es) the completion.
+        --  This will auto-import if your LSP supports it.
+        --  This will expand snippets if the LSP sent a snippet.
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+        -- Manually trigger a completion from nvim-cmp.
+        --  Generally you don't need this, because nvim-cmp will display
+        --  completions whenever it has completion options available.
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
+
+        -- Think of <c-l> as moving to the right of your snippet expansion.
+        --  So if you have a snippet that's like:
+        --  function $name($args)
+        --    $body
+        --  end
+        --
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
-          else
-            fallback()
           end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
-          else
-            fallback()
           end
         end, { 'i', 's' }),
       },
@@ -77,5 +88,5 @@ return {
         { name = 'path' },
       },
     }
-  end
+  end,
 }
