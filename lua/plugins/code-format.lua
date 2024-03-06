a8edb20e8a30fdfc2f4118fd1bd373441abd7775
marinjuricev <mjuric00@fesb.hr>
2024-03-06 22:42:41 +0100

    chore: Add a surrond.nvim

--- lua/plugins/code-format.lua
+++ lua/plugins/code-format.lua
@@ -10,6 +10,16 @@ return {
     main = 'ibl',
     opts = {},
   },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   -- "gc" to comment visual regions/lines
