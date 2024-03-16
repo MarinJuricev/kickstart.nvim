ce82c7378455e54ed38eef304bf58d38484ef71a
marinjuricev <mjuric00@fesb.hr>
2024-03-16 09:21:46 +0100

    chore: Add the ability to exlude some language from the format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -28,8 +28,14 @@ return {
         ignore_install = {},
         -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
         modules = {},
-        highlight = { enable = true },
-        indent = { enable = true },
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
         incremental_selection = {
           enable = true,
           keymaps = {
