dbfb4d52467dadcfb521e1f6d7fe1d7b37fdb9a4
marinjuricev <mjuric00@fesb.hr>
2024-04-14 14:28:18 +0200

    chore: Add the lazy tag on comment lazy load them

--- lua/plugins/code-format.lua
+++ lua/plugins/code-format.lua
@@ -23,5 +23,5 @@ return {
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   --  "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim', event = 'VeryLazy', opts = {} },
 }
