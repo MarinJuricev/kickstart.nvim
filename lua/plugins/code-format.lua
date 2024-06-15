de0dbe6deba3b228906b93ad882e4681408781e1
marinjuricev <mjuric00@fesb.hr>
2024-06-15 09:22:30 +0200

    chore: nvim 0.10 added a inbuilt commentary plugin, play around with it, also [d]d are default mappings

--- lua/plugins/code-format.lua
+++ lua/plugins/code-format.lua
@@ -22,6 +22,4 @@ return {
   },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  --  "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', event = 'VeryLazy', opts = {} },
 }
