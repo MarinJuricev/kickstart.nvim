1278f01e57b6950efc528afcd5495bfbc0873f81
marinjuricev <mjuric00@fesb.hr>
2024-06-15 09:57:26 +0200

    chore: Add a config for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,4 +3,16 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('oil').setup {
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+      win_options = {
+        wrap = false,
+      },
+    }
+  end,
 }
