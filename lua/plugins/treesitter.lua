8e9ba4b2385fab167a847ac17fcc0bf4d4df041c
marinjuricev <mjuric00@fesb.hr>
2024-08-24 07:36:56 +0200

    chore: Update which key and treesitter to match kickstart
    I could/should revisit the treesitter setup, it seems rather bloated atm

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,6 +11,8 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = function()
     -- [[ Configure Treesitter ]]
     -- See `:help nvim-treesitter`
