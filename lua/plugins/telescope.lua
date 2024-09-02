75a548d5d142a8e5313664610cfd633a4f6736a6
marinjuricev <mjuric00@fesb.hr>
2024-09-02 18:39:16 +0200

    chore: Update the telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,6 +37,25 @@ return {
   },
   config = function()
     -- Enable telescope extensions, if they are installed
+    -- [[ Configure Telescope ]]
+    -- See `:help telescope` and `:help telescope.setup()`
+    require('telescope').setup {
+      -- You can put your default mappings / updates / etc. in here
+      --  All the info you're looking for is in `:help telescope.setup()`
+      --
+      -- defaults = {
+      --   mappings = {
+      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+      --   },
+      -- },
+      -- pickers = {}
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
+        },
+      },
+    }
+
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
     -- Telescope live_grep in git root
