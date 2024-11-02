16e95ab73c341bcb1bcbdc25addcbc3f16187d03
marinjuricev <mjuric00@fesb.hr>
2024-11-02 08:07:23 +0100

    chore: Align with kickstart

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -42,29 +42,24 @@ return {
     -- lint.linters_by_ft['terraform'] = nil
     -- lint.linters_by_ft['text'] = nil
 
+    local function try_run_linter()
+      -- Only run the linter in buffers that you can modify to
+      -- avoid superfluous noise, notably within the handy LSP pop-ups that
+      -- describe the hovered symbol using Markdown.
+      if vim.opt_local.modifiable:get() then
+        lint.try_lint()
+      end
+    end
+
     -- Create autocommand which carries out the actual linting
     -- on the specified events.
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
-      callback = function()
-        -- Only run the linter in buffers that you can modify in order to
-        -- avoid superfluous noise, notably within the handy LSP pop-ups that
-        -- describe the hovered symbol using Markdown.
-        if vim.opt_local.modifiable:get() then
-          lint.try_lint()
-        end
-      end,
+      callback = try_run_linter,
     })
 
-    vim.keymap.set('n', '<leader>l', function()
-      -- Only run the linter in buffers that you can modify in order to
-      -- avoid superfluous noise, notably within the handy LSP pop-ups that
-      -- describe the hovered symbol using Markdown.
-      if vim.opt_local.modifiable:get() then
-        lint.try_lint()
-      end
-    end, { desc = 'Trigger linting for current file' })
+    vim.keymap.set('n', '<leader>l', try_run_linter, { desc = 'Trigger linting for current file' })
   end,
 }
