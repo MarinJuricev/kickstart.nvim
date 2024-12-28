a8698d487b53cba55990b8d635b990826f3282e4
marinjuricev <mjuric00@fesb.hr>
2024-12-28 18:32:40 +0100

    chore: Look into the AI plugins

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,6 @@
+return {
+  -- Look into:
+  -- : SuperMaven https://github.com/supermaven-inc/supermaven-nvim
+  -- : gp.nvim https://github.com/Robitx/gp.nvim
+  -- : avante.nvim https://github.com/yetone/avante.nvim
+}
