04e2bc61256ad4ca9b459acc385c12c9d8b3ca49
marinjuricev <mjuric00@fesb.hr>
2025-01-08 08:52:23 +0100

    bugfix: Avoid calling setup twice for Mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,8 +20,9 @@ return {
     'neovim/nvim-lspconfig',
     event = 'VeryLazy',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      -- Mason must be loaded before its dependents so we need to set it up here.
+      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+      { 'williamboman/mason.nvim', opts = {} },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       -- Allows extra capabilities provided by nvim-cmp
@@ -212,8 +213,11 @@ return {
       --  other tools, you can run
       --    :Mason
       --
-      --  You can press `g?` for help in this menu
-      require('mason').setup()
+      -- You can press `g?` for help in this menu.
+      --
+      -- `mason` had to be setup earlier: to configure its options see the
+      -- `dependencies` table for `nvim-lspconfig` above.
+      --
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
