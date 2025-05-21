bc0ae69c255bc4107d080ba5983ab67b768249d7
marinjuricev <mjuric00@fesb.hr>
2025-05-21 20:19:41 +0200

    feat: Replace the epwalsh obsidian plugin with the community one

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,5 @@
 return {
-  'epwalsh/obsidian.nvim',
+  'obsidian-nvim/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = 'markdown',
@@ -58,7 +58,9 @@ return {
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
       -- Set to false to disable completion.
-      nvim_cmp = true,
+      nvim_cmp = false,
+      -- Set to false to disable completion.
+      blink = true,
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
