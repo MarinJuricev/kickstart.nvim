c483c286968ef183e97395e3166085ebef00e772
marinjuricev <mjuric00@fesb.hr>
2025-08-17 19:02:47 +0200

    feat: Add the search term in visual mode aswell

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -50,7 +50,7 @@ return {
   },
   -- Document existing key chains
   spec = {
-    { '<leader>s', group = '[S]earch' },
+    { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
     { '<leader>t', group = '[T]oggle' },
     { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
     { 'gr', group = 'LSP Actions', mode = { 'n' } },
